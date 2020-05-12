1460e70a72c213c1f73723644a2fa84144e62a19
GreenWitchCup <jad.bennouna@outlook.fr>
2020-05-11 21:17:18 -0400

    respNavBar to be continued...

--- ToolBar.js
+++ ToolBar.js
@@ -1,3 +1,15 @@
 import React from 'react';
 
-const toolBar  = props =>
+const toolBar  = props => (
+    <header>
+        <nav>
+            <div></div>
+            <div><a href=""></a>The logo</div>
+            <div>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+)
